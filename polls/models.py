327550780b7310def4bd7bce07e0611753f8bd4c
Mohit Khaitan <khaitanmkkmohit@gmail.com>
2019-11-08 13:19:01 +0530

    created models and migration

--- polls/models.py
+++ polls/models.py
@@ -1,3 +1,42 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Poll(models.Model):
+    """
+    Poll Model to store Polls data
+    """
+    question = models.CharField(max_length=100)
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE)
+    pub_date = models.DateField(auto_now=True)
+
+    def __str__(self: str) -> str:
+        """
+        Returns the questions associated with a poll
+        :return: Question associated with the poll
+        """
+        return self.question
+
+
+class Choice(models.Model):
+    """
+    Choice model to store choices of a Poll
+    """
+    poll = models.ForeignKey(Poll, related_name='choices', on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=100)
+
+    def __str__(self: str) -> str:
+        """
+        Returns the choice_text associated with the poll question
+        :return:
+        """
+        return self.choice_text
+
+
+class Vote(models.Model):
+    """
+    Vote model to store the count of vote per poll
+    """
+    choice = models.ForeignKey(Choice, related_name='votes', on_delete=models.CASCADE)
+    poll = models.ForeignKey(Poll, on_delete=models.CASCADE)
+    voted_by = models.ForeignKey(User, on_delete=models.CASCADE)
