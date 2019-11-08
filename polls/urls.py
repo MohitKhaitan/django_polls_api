0e79d4cfc1d9e2409a7f33b115cba3affe63bb08
Mohit Khaitan <khaitanmkkmohit@gmail.com>
2019-11-08 18:56:34 +0530

    bug fixes

--- polls/urls.py
+++ polls/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     path('polls/', PollList.as_view(), name='polls_list'),
     path('polls/<int:pk>/', PollDetail.as_view(), name='polls_detail'),
     path('polls/<int:pk>/choice/', ChoiceList.as_view(), name='choice_list'),
-    path('polls/<int:pk>/choice/<int:choice_pk>/vote/', CreateVote.as_view(), name='create_vote')
+    path('polls/<int:pk>/choice/<int:choice_pk>/vote/', CreateVote.as_view(), name='create_vote'),
 ]
