2fe42434c42279246aa3af3bfb0b30095c97622e
Mohit Khaitan <khaitanmkkmohit@gmail.com>
2019-11-22 17:30:32 +0530

    Added logging feature

--- polls/apiviews.py
+++ polls/apiviews.py
@@ -5,6 +5,13 @@ from rest_framework import status
 from django.shortcuts import get_object_or_404
 from .models import Poll, Choice
 from .serializers import PollSerializer, ChoiceSerializer, VoteSerializer
+import logging
+
+logging.basicConfig(filename='pollsLogFile.txt',
+                    filemode='a',
+                    format='%(asctime)s %(levelname)s - %(message)s',
+                    datefmt='%Y-%m-%d %H:%M:%S',
+                    level=logging.DEBUG)
 
 
 class PollList(APIView):
@@ -18,9 +25,17 @@ class PollList(APIView):
         :param request: Http request object
         :return: Response Object. List of polls. MAX 20
         """
+        logging.info("Inside Poll List Endpoint")
+        logging.debug("Poll List Request: {}".format(request.data))
+
         polls = Poll.objects.all()[:20]
+        logging.debug("PollQueryList: {}".format(polls))
+
         data = PollSerializer(polls, many=True).data
-        return Response(data=data, status=status.HTTP_200_OK, content_type=request.content_type)
+
+        logging.debug("Response: {}".format(Response(data=data, status=status.HTTP_200_OK,
+                                                     content_type='application/json').data))
+        return Response(data=data, status=status.HTTP_200_OK, content_type='application/json')
 
 
 class PollDetail(APIView):
@@ -35,8 +50,16 @@ class PollDetail(APIView):
         :param pk: Poll id for which details need to be fetched
         :return: Response Object. Poll details
         """
+        logging.info("Inside Poll Detail Endpoint")
+        logging.debug("Poll Detail Request: {}".format(request.data))
+
         poll = get_object_or_404(Poll, pk=pk)
+        logging.debug("Poll Detail: {}".format(poll))
+
         data = PollSerializer(poll).data
+
+        logging.debug("Poll Detail Response: {}".format(Response(data=data, status=status.HTTP_200_OK,
+                                                                 content_type=request.content_type)))
         return Response(data=data, status=status.HTTP_200_OK, content_type=request.content_type)
 
 
@@ -52,8 +75,16 @@ class ChoiceList(APIView):
         :param pk: Poll id for which choices needs to be fetched
         :return: Response Object. Choice list
         """
+        logging.info("Inside Choice List Endpoint")
+        logging.debug("Choice List Request: {}".format(request.data))
+
         choice = Choice.objects.filter(poll_id=pk)
+        logging.debug("Choice List: {}".format(choice))
+
         data = ChoiceSerializer(choice, many=True).data
+
+        logging.debug("Choice List Response: {}".format(Response(data=data, status=status.HTTP_200_OK,
+                                                                 content_type=request.content_type)))
         return Response(data=data, status=status.HTTP_200_OK, content_type=request.content_type)
 
 
@@ -70,7 +101,13 @@ class CreateVote(APIView):
         :param choice_pk: Choice id for which vote needed to be cast
         :return: Response Object. Success message
         """
+        logging.info("Inside Create Vote Endpoint")
+        logging.debug("Create Vote Request: {}".format(request.data))
+        logging.debug("Create Vote pk: {}".format(pk))
+        logging.debug("Create Vote choice_pk: {}".format(choice_pk))
+
         voted_by = request.data.get('voted_by')
+
         data = {
             'choice': choice_pk,
             'poll': pk,
@@ -79,7 +116,10 @@ class CreateVote(APIView):
         vote_serializer = VoteSerializer(data=data)
         if vote_serializer.is_valid():
             vote_serializer.save()
+            logging.debug("Create Vote Response: {}".format(Response(vote_serializer.data, status=status.HTTP_200_OK,
+                                                                     content_type=request.content_type)))
             return Response(vote_serializer.data, status=status.HTTP_200_OK, content_type=request.content_type)
         else:
+            logging.debug("Create Vote Response: {}".format(vote_serializer.errors))
             return Response(vote_serializer.errors, status=status.HTTP_400_BAD_REQUEST,
                             content_type=request.content_type)
--- pollsapi/urls.py
+++ pollsapi/urls.py
@@ -15,9 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include, re_path
-from rest_framework.documentation import include_docs_urls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     re_path(r'^', include('polls.urls'))
 ]
+
